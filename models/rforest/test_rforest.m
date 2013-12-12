74d840032e2afdea5719207228ff9db9f54863c3
Dieterich Lawson <dieterich.lawson@gmail.com>
2013-12-12 10:33:49 -0800

    updates to rforest results

--- models/rforest/test_rforest.m
+++ models/rforest/test_rforest.m
@@ -1,8 +1,6 @@
 clear;
 imp = importdata('../../data/gold/taps_accel.csv',',',1);
 names = imp.textdata(2:end,1);
-% data is poorly conditioned because of extra
-% features at the end, so remove them
 all_data = [grp2idx(names) imp.data(:,1:end)];
 
 pins = unique(all_data(:,2));
